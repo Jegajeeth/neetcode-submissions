83c7c34139a773bd7d1e15579300e3eae253403c
Jegajeeth Manikandan <85186073+Jegajeeth@users.noreply.github.com>
2026-07-09 17:32:45 +0530

    Add: valid-sudoku - submission-1

---
+++ Data Structures & Algorithms/valid-sudoku/submission-1.cs
@@ -0,0 +1,60 @@
+public class Solution {
+    public bool IsValidSudoku(char[][] board) {
+        HashSet<char> rowHash = new();
+        HashSet<char> colHash = new();
+        Dictionary<string, HashSet<char>> squareCheck = new();
+        char rowElement;
+        char colElement;
+        for(int i = 0; i < board.Length; i++)
+        {
+            for(int j = 0; j < board.Length; j++)
+            {
+                rowElement = board[i][j];
+                colElement = board[j][i];
+                string rowSquare = (i/3 + "") + (j/3 + "");
+                string colSquare = (j/3 + "") + (i/3 + "");
+                if(!squareCheck.ContainsKey(rowSquare))
+                {
+                    squareCheck[rowSquare] = new HashSet<char>();
+                }
+                if(!squareCheck.ContainsKey(colSquare))
+                {
+                    squareCheck[colSquare] = new HashSet<char>();
+                }
+                if(rowElement != '.')
+                {
+                    if(!(rowHash.Add(rowElement)))
+                    {
+                        return false;
+                    }
+
+                    // exclude already checked row and column elements along with the intersecting element,
+                    // for the sub-square in a sudoko.
+                    if(i < j && (!(squareCheck[rowSquare].Add(rowElement))))
+                    {
+                        return false;
+                    }
+                }
+                if(colElement != '.')
+                {
+                    if(!(colHash.Add(colElement)))
+                    {
+                        return false;
+                    }
+
+                    // exclude already checked row and column elements,
+                    // include the common shared row and column intersecting element. 
+                    // because, the element should atleast be added once in the check heap table.
+                    if(i <= j && (!(squareCheck[colSquare].Add(colElement))))
+                    {
+                        return false;
+                    }
+                }
+            }
+            rowHash = new();
+            colHash = new();
+        }
+
+        return true;
+    }
+}
